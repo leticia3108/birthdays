456997f18de2aa5b00686820fe6f2029b0290ad7
Leticia <leticia-crepaldi@usp.br>
2025-10-14 00:03:10 -0300

    Aniversarios ordenados

---
+++ lib/screens/add_screen.dart
@@ -0,0 +1,181 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AddScreen extends StatefulWidget {
+  DateTime selectedDate = DateTime.now();
+  final Function callback;
+  final Function setName;
+  final Function setDate;
+  final TextEditingController nameControl;
+
+  AddScreen({
+    super.key,
+    required this.callback,
+    required this.setName,
+    required this.setDate,
+    required this.nameControl,
+  });
+
+  @override
+  State<AddScreen> createState() => _AddScreenState();
+}
+
+class _AddScreenState extends State<AddScreen> {
+  DateTime selectedDate = DateTime.now();
+  DateTime initialDate = DateTime.now();
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+
+  Future<void> _pickImage() async {
+    final XFile? pickedFile = await _picker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = pickedFile;
+      });
+      // You can now use _image.path to access the image file path
+      // For example, to display it: Image.file(File(_image!.path))
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Adicione aniversariantes!',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            fontFamily: 'MountainsofChristmas',
+            letterSpacing: 1,
+          ),
+        ),
+        backgroundColor: Colors.teal,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    _pickImage();
+                  },
+                  child: _image != null
+                      ? CircleAvatar(
+                          minRadius: 50,
+                          backgroundImage: FileImage(File(_image!.path)),
+                        )
+                      : const Text('Escolha uma imagem!'), // Mudar isso
+                  style: ElevatedButton.styleFrom(shape: const CircleBorder()),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 25),
+                      Container(
+                        height: 50,
+                        margin: EdgeInsets.only(right: 25),
+                        child: TextField(
+                          controller: widget.nameControl,
+                          onChanged: (text) =>
+                              widget.setName(widget.nameControl.text),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Nome',
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 25),
+                      //Text("$selectedDate.day"),
+                      Container(
+                        margin: EdgeInsets.only(right: 25),
+                        child: Text(
+                          "Data de nascimento: ",
+                          style: TextStyle(
+                            color: Colors.teal,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                            fontFamily: 'MountainsofChristmas',
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 25),
+                        child: ElevatedButton.icon(
+                          label: Text(
+                            "${selectedDate.day.toString().padLeft(2, '0')}/${selectedDate.month.toString().padLeft(2, '0')}/${selectedDate.year}",
+                            style: TextStyle(
+                              color: Colors.teal,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              fontFamily: 'MountainsofChristmas',
+                              letterSpacing: 1,
+                            ),
+                          ),
+                          icon: Icon(Icons.date_range),
+                          style: ButtonStyle(
+                            shape:
+                                WidgetStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                    side: BorderSide(color: Colors.teal),
+                                  ),
+                                ),
+                          ),
+                          onPressed: () async {
+                            var tempSelectedDate = await showDatePicker(
+                              context: context,
+                              initialDate: initialDate,
+                              firstDate: DateTime(1950),
+                              lastDate: DateTime(2150),
+                            );
+
+                            if (tempSelectedDate != null) {
+                              setState(() {
+                                selectedDate = tempSelectedDate;
+                              });
+                              widget.setDate(selectedDate);
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 25),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 25),
+              child: TextField(
+                maxLines: 10,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Descrição',
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            FloatingActionButton(
+              onPressed: () {
+                widget.callback();
+                Navigator.pop(context);
+              },
+              child: const Icon(Icons.add),
+              backgroundColor: Colors.teal,
+              shape: CircleBorder(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:birthdays/screens/add_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<List<Object>> register = [
+    ['Daniel', 21, 07, 2001],
+    ['Leticia', 31, 08, 2001],
+    ['Marina', 14, 02, 2001],
+    ['Maira', 7, 06, 2001],
+    ['Amanda', 7, 01, 2001],
+    ['Natalie', 5, 12, 2001],
+    ['Cássio', 23, 06, 2001],
+  ];
+
+  var tempName = "NoN";
+  DateTime data = DateTime.now();
+  final TextEditingController nameControl = TextEditingController();
+
+  @override
+  void dispose() {
+    nameControl.dispose();
+    super.dispose();
+  }
+
+  Future<String> getFilePath() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return '${dir.path}/dados.json';
+  }
+
+  Future<void> saveJson(List<List<Object>> register) async {
+    final path = await getFilePath();
+    final file = File(path);
+    await file.writeAsString(jsonEncode(register));
+  }
+
+  Future<void> loadJson() async {
+    final path = await getFilePath();
+    final file = File(path);
+
+    if (await file.exists()) {
+      String contents = await file.readAsString();
+      var data = jsonDecode(contents);
+      register = List<List<Object>>.from(data.map((e) => List<Object>.from(e)));
+    } else {
+      print("File does not exist.");
+    }
+  }
+
+  void setName(String nome) {
+    tempName = nome;
+  }
+
+  void setDate(DateTime dataChild) {
+    if (dataChild != Null) {
+      data = dataChild;
+    }
+  }
+
+  void _addMember() async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddScreen(
+          callback: onChanged,
+          setName: setName,
+          setDate: setDate,
+          nameControl: nameControl,
+        ),
+      ),
+    );
+
+    setState(() {});
+  }
+
+  void onChanged() {
+    register.add([tempName, data.day, data.month, data.year]);
+    print('Hi');
+    saveJson(register);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    saveJson(register);
+    loadJson();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    register.sort((a, b) {
+      var aData, bData;
+      if ((a[2] as int) * 100 + (a[1] as int) - data.month * 100 - data.day >=
+          0) {
+        aData =
+            (a[2] as int) * 100 + (a[1] as int) - data.month * 100 - data.day;
+      } else {
+        aData =
+            9999 +
+            (a[2] as int) * 100 +
+            (a[1] as int) +
+            -data.month * 100 -
+            data.day;
+      }
+
+      if ((b[2] as int) * 100 + (b[1] as int) - data.month * 100 - data.day >=
+          0) {
+        bData =
+            (b[2] as int) * 100 + (b[1] as int) - data.month * 100 - data.day;
+      } else {
+        bData =
+            9999 +
+            ((b[2] as int) * 100 + (b[1] as int) - data.month * 100 - data.day);
+      }
+
+      return aData.compareTo(bData);
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: ListView.separated(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          itemBuilder: (context, position) {
+            return Row(
+              spacing: 20,
+              children: <Widget>[
+                CircleAvatar(backgroundImage: AssetImage('images/cake.jpg')),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      register[position][0].toString(),
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'MountainsofChristmas',
+                        fontSize: 20,
+                        height: 3,
+                        letterSpacing: 1,
+                      ),
+                    ),
+                    Text(
+                      register[position][1].toString().padLeft(2, '0') +
+                          '/' +
+                          register[position][2].toString().padLeft(2, '0') +
+                          '/' +
+                          register[position][3].toString().padLeft(2, '0'),
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'MountainsofChristmas',
+                        fontSize: 20,
+                        height: 1.5,
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+          separatorBuilder: (context, position) {
+            return Container(height: 20);
+          },
+          itemCount: register.length,
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(
+          'Aniversários',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            fontFamily: 'MountainsofChristmas',
+            letterSpacing: 1,
+          ),
+        ),
+        backgroundColor: Colors.teal,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addMember,
+        tooltip: 'Adicione um membro!',
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.teal,
+        shape: CircleBorder(),
+      ),
+    );
+  }
+}
